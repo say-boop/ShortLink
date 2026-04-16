37311ab56f7e872bdb1a7fe0b82c3a02a669816d
say-boop <say284821@gmail.com>
2026-04-16 17:56:43 +0500

    ready

---
+++ app/schemas/link.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel, field_validator
+import re
+import datetime
+
+
+class LinkCreate(BaseModel):
+  original_url: str
+  
+  @field_validator('original_url')
+  @classmethod
+  def validate_url(cls, v: str) -> str:
+    if v is None:
+      return v
+    
+    pattern = r"^https?://[^\s/$.?#].[^\s]*$"
+    
+    if not re.match(pattern, v):
+      raise ValueError('Некорректный URL')
+    return v
+
+class LinkResponse(BaseModel):
+  id: int
+  short_code: str
+  original_url: str
+  clicks: int
+  created_at: datetime.datetime
+  
+  model_config = {
+    "from_attributes": True
+  }
+
+class LinkStats(BaseModel):
+  short_code: str
+  original_url: str
+  clicks: int
+  created_at: datetime.datetime
