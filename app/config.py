e10bc8626bd619469d92fcea7bf109aafde4f0ad
say-boop <say284821@gmail.com>
2026-05-02 14:41:25 +0500

    ready-redis

--- app/config.py
+++ app/config.py
@@ -6,6 +6,8 @@ class Settings(BaseSettings):
   SECRET_KEY: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
   ALGORITHM: str = "HS256"
   ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+  REDIS_HOST: str = "redis"
+  REDIS_PORT: int = 6379
   
   model_config = ConfigDict(
     env_file=".env",
